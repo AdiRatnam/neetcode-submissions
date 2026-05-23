83702b7f4fb9cc5593c128dacb36a87c7c934bf9
AdiRatnam <ratnamadi2005@gmail.com>
2026-05-23 19:17:41 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!= len(t):
+            return False
+        else:
+            s = s.lower()
+            t = list(t.lower())
+           
+            for i in range (len(s)):
+                if s[i] in t:
+                    t.remove(s[i])
+                
+            if t:
+                return False
+            else:
+                return True
+        
